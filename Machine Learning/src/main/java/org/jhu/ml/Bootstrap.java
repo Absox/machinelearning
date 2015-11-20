98f9cf6ec0963519d1ae3803c8c03d3037ee0294
Abso <rliu14@jhu.edu>
2015-11-19 23:20:16 -0500

    Initial Commit.

---
+++ Machine Learning/src/main/java/org/jhu/ml/Bootstrap.java
@@ -0,0 +1,12 @@
+package org.jhu.ml;
+
+/**
+ * Created by Ran on 11/19/2015.
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Machine Learning/src/main/java/org/jhu/ml/model/FiringSolution.java
@@ -0,0 +1,7 @@
+package org.jhu.ml.model;
+
+/**
+ * Created by Ran on 11/19/2015.
+ */
+public class FiringSolution {
+}
