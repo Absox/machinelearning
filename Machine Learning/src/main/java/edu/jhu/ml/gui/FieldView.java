7564d564e80d38846b3c2f4919595550597d3751
Abso <rliu14@jhu.edu>
2015-11-23 17:07:00 -0500

    Implementation of GUI code. Still in progress.

--- Machine Learning/src/main/java/edu/jhu/ml/gui/FieldView.java
+++ Machine Learning/src/main/java/edu/jhu/ml/gui/FieldView.java
@@ -22,11 +22,19 @@ public class FieldView extends JPanel implements Observer {
         this.model = model;
     }
 
+    protected void paintComponent(Graphics g) {
+        //this.setBackground(Color.BLACK);
+
+        g.drawString("This is my custom Panel!",10,20);
+    }
+
     /**
      * Draws the field view.
      * @param g
      */
     public void paint(Graphics g) {
+
+        this.setBackground(Color.black);
 
     }
 
@@ -45,5 +53,13 @@ public class FieldView extends JPanel implements Observer {
      */
     private void draw(GraphicalRepresentation g) {
         // TODO
+    }
+
+    /**
+     * Gets preferred size for this view.
+     * @return Preferred size.
+     */
+    public Dimension getPreferredSize() {
+        return new Dimension(1067, 600);
     }
 }
--- Machine Learning/src/main/java/edu/jhu/ml/gui/FieldWindow.java
+++ Machine Learning/src/main/java/edu/jhu/ml/gui/FieldWindow.java
@@ -3,7 +3,6 @@ package edu.jhu.ml.gui;
 import edu.jhu.ml.model.FieldModel;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Window class.
@@ -12,8 +11,14 @@ import java.awt.*;
 public class FieldWindow extends JFrame {
 
     public FieldWindow(FieldModel model) {
-        this.setSize(new Dimension(1067, 600));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FieldView fieldView = new FieldView(model);
+
+        this.setContentPane(fieldView);
+        this.pack();
+
         this.setVisible(true);
+
     }
 }
