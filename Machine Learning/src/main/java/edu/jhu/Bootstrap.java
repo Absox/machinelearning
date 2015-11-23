8def1781fea8320944a06665939f33fce4037c2b
Abso <rliu14@jhu.edu>
2015-11-23 13:05:13 -0500

    Initial Commit.

---
+++ Machine Learning/src/main/java/edu/jhu/Bootstrap.java
@@ -0,0 +1,7 @@
+package edu.jhu;
+
+/**
+ * Created by Ran on 11/23/2015.
+ */
+public class Bootstrap {
+}
--- Machine Learning/src/main/java/edu/jhu/ml/config/Configurations.java
+++
@@ -1,7 +0,0 @@
-package edu.jhu.ml.config;
-
-/**
- * Created by Ran on 11/19/2015.
- */
-public abstract class Configurations {
-}
