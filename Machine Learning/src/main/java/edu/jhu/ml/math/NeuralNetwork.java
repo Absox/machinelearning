c66883158bcbc192d7540fafc4809383fce5b3ed
Abso <rliu14@jhu.edu>
2015-12-07 23:59:43 -0500

    Neural net learner implementation.

--- Machine Learning/src/main/java/edu/jhu/ml/math/NeuralNetwork.java
+++ Machine Learning/src/main/java/edu/jhu/ml/math/NeuralNetwork.java
@@ -1,6 +1,11 @@
 package edu.jhu.ml.math;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+import java.util.Random;
 
 /**
  * Created by Ran on 12/7/2015.
@@ -9,9 +14,10 @@ public class NeuralNetwork {
 
     private int numInputs;
     private int numOutputs;
-    private int[] numHiddenLayerNodes;
+    private double learningRate;
 
-    RealMatrix[] weightMatrices;
+    private int[] numHiddenLayerNodes;
+    private RealMatrix[] weightMatrices;
 
     /**
      * Constructs a neural network with the following characteristics:
@@ -19,9 +25,21 @@ public class NeuralNetwork {
      * @param numHiddenLayerNodes An array of ints containing the number of hidden layer nodes on each layer.
      * @param numOutputs Number of outputs;
      */
-    public NeuralNetwork(int numInputs, int[] numHiddenLayerNodes, int numOutputs) {
+    public NeuralNetwork(int numInputs, int[] numHiddenLayerNodes, int numOutputs, double learningRate) {
+        this.numInputs = numInputs;
+        this.numOutputs = numOutputs;
+        this.numHiddenLayerNodes = numHiddenLayerNodes;
+        this.learningRate = learningRate;
+
         // We require 1 weight matrix for every hidden layer, and then one for the output layer.
         this.weightMatrices = new RealMatrix[numHiddenLayerNodes.length + 1];
+        this.weightMatrices[0] = new Array2DRowRealMatrix(numHiddenLayerNodes[0], numInputs);
+        for (int c = 1; c < numHiddenLayerNodes.length; c++) {
+            this.weightMatrices[c] = new Array2DRowRealMatrix(numHiddenLayerNodes[c-1], numHiddenLayerNodes[c]);
+        }
+        this.weightMatrices[numHiddenLayerNodes.length] = new Array2DRowRealMatrix(numOutputs, numHiddenLayerNodes[numHiddenLayerNodes.length - 1]);
+
+        this.initialize();
     }
 
     /**
@@ -29,6 +47,82 @@ public class NeuralNetwork {
      */
     public void initialize() {
 
+        Random rng = new Random(0);
+        for (int c = 0; c < weightMatrices.length; c++) {
+            RealMatrix currentWeightMatrix = weightMatrices[c];
+            for (int i = 0; i < currentWeightMatrix.getRowDimension(); i++) {
+                for (int j = 0; j < currentWeightMatrix.getColumnDimension(); j++) {
+                    currentWeightMatrix.setEntry(i, j, rng.nextDouble());
+                }
+            }
+        }
+
+    }
+
+    public double[] getOutput(double[] inputs) {
+        State state = new State(inputs);
+        return state.output.toArray();
+    }
+
+    /**
+     * Updates weight matrices via the backpropagation algorithm.
+     * @param inputs
+     * @param truths
+     */
+    public void learn(double[] inputs, double[] truths) {
+        if (inputs.length != numInputs || truths.length != numOutputs) return;
+        State state = new State(inputs);
+
+        // Calculate deltas
+        RealVector[] deltas = new RealVector[numHiddenLayerNodes.length + 1];
+        deltas[numHiddenLayerNodes.length] = new ArrayRealVector(numOutputs);
+        for (int c = 0; c < numOutputs; c++) {
+            deltas[numHiddenLayerNodes.length].setEntry(c, (state.output.getEntry(c) - truths[c]) * (state.output.getEntry(c) - Math.pow(state.output.getEntry(c), 2)));
+        }
+        for (int n = numHiddenLayerNodes.length - 1; n >= 0; n--) {
+            deltas[n] = new ArrayRealVector(numHiddenLayerNodes[n]);
+            for (int c = 0; c < numHiddenLayerNodes[n]; c++) {
+                deltas[n].setEntry(c, (state.hiddenLayers[n].getEntry(c) - Math.pow(state.hiddenLayers[n].getEntry(c), 2)) * weightMatrices[n+1].getColumnVector(c).dotProduct(deltas[n+1]));
+            }
+        }
+
+        // Calculate updates
+        RealMatrix[] gradients = new RealMatrix[weightMatrices.length];
+        for (int n = weightMatrices.length - 1; n >= 1; n--) {
+            gradients[n] = new Array2DRowRealMatrix(weightMatrices[n].getRowDimension(), weightMatrices[n].getColumnDimension());
+            for (int c = 0; c < weightMatrices[n].getRowDimension(); c++) {
+                for (int d = 0; d < weightMatrices[n].getColumnDimension(); d++) {
+                    gradients[n].setEntry(c, d, deltas[n].getEntry(c) * state.hiddenLayers[n-1].getEntry(d));
+                }
+            }
+        }
+        gradients[0] = new Array2DRowRealMatrix(weightMatrices[0].getRowDimension(), weightMatrices[0].getColumnDimension());
+        for (int c = 0; c < weightMatrices[0].getRowDimension(); c++) {
+            for (int d = 0; d < weightMatrices[0].getColumnDimension(); d++) {
+                gradients[0].setEntry(c, d, deltas[0].getEntry(c) * inputs[d]);
+            }
+        }
+
+        // Apply updates
+        for (int n = 0; n < weightMatrices.length; n++) {
+            weightMatrices[n] = weightMatrices[n].add(gradients[n].scalarMultiply(-learningRate));
+        }
+
+    }
+
+    public int getClass(double[] input) {
+        if (input.length != numInputs) return -1;
+        double[] outputs = this.getOutput(input);
+
+        int index = 0;
+        double currentMax = outputs[0];
+        for (int c = 1; c < numInputs; c++) {
+            if (outputs[c] > currentMax) {
+                currentMax = outputs[c];
+                index = c;
+            }
+        }
+        return index;
     }
 
     /**
@@ -38,5 +132,35 @@ public class NeuralNetwork {
      */
     private static double activationFunction(double z) {
         return 1 / (1 + Math.exp(-z));
+    }
+
+    private class State {
+
+        private RealVector[] hiddenLayers;
+        private RealVector output;
+
+        public State(double[] inputs) {
+            this(new ArrayRealVector(inputs));
+        }
+
+        public State(RealVector input) {
+            this.hiddenLayers = new RealVector[numHiddenLayerNodes.length];
+            this.output = new ArrayRealVector(numOutputs);
+
+            this.hiddenLayers[0] = new ArrayRealVector(numHiddenLayerNodes[0]);
+            for (int c = 0; c < numHiddenLayerNodes[0]; c++) {
+                hiddenLayers[0].setEntry(c, activationFunction(weightMatrices[0].getRowVector(c).dotProduct(input)));
+            }
+            for (int n = 1; n < numHiddenLayerNodes.length; n++) {
+                hiddenLayers[n] = new ArrayRealVector(numHiddenLayerNodes[n]);
+                for (int c = 0; c < numHiddenLayerNodes[n]; c++) {
+                    hiddenLayers[n].setEntry(c, activationFunction(weightMatrices[n].getRowVector(c).dotProduct(hiddenLayers[n-1])));
+                }
+            }
+            for (int c = 0; c < numOutputs; c++) {
+                output.setEntry(c, activationFunction(weightMatrices[numHiddenLayerNodes.length].getRowVector(c).dotProduct(hiddenLayers[numHiddenLayerNodes.length - 1])));
+            }
+        }
+
     }
 }
--- Machine Learning/src/test/java/SingleHiddenLayerNNTest.java
+++ Machine Learning/src/test/java/SingleHiddenLayerNNTest.java
@@ -1,3 +1,4 @@
+import edu.jhu.ml.math.NeuralNetwork;
 import edu.jhu.ml.math.SingleHiddenLayerNeuralNetwork;
 import org.junit.Test;
 
@@ -11,19 +12,15 @@ public class SingleHiddenLayerNNTest {
     @Test
     public void testValues() {
 
-        SingleHiddenLayerNeuralNetwork testNN = new SingleHiddenLayerNeuralNetwork(2, 5, 2, 1);
+        SingleHiddenLayerNeuralNetwork testNN = new SingleHiddenLayerNeuralNetwork(2, 5, 2, 5);
         double[] testValues = {0, 1};
-        double[] testTruths = {0, 1};
-
         double[] testValues2 = {1, 0};
-        double[] testTruths2 = {1, 0};
-
         double[] testValues3 = {1, 1};
         double[] testValues4 = {0, 0};
 
-        for (int c = 0; c < 100; c++) {
-            testNN.learn(testValues, testTruths);
-            testNN.learn(testValues2, testTruths2);
+        for (int c = 0; c < 10; c++) {
+            testNN.learn(testValues, testValues);
+            testNN.learn(testValues2, testValues2);
             testNN.learn(testValues3, testValues3);
             testNN.learn(testValues4, testValues4);
         }
@@ -36,5 +33,29 @@ public class SingleHiddenLayerNNTest {
         System.out.println(testNN.getClass(testValues));
         System.out.println(testNN.getClass(testValues2));
 
+    }
+
+    @Test
+    public void testGeneralNeuralNetwork() {
+        int[] numHiddenLayerNodes = {5};
+        NeuralNetwork neuralNetwork = new NeuralNetwork(2, numHiddenLayerNodes, 2, 5);
+        double[] testValues = {0, 1};
+        double[] testValues2 = {1, 0};
+        double[] testValues3 = {1, 1};
+        double[] testValues4 = {0, 0};
+
+        for (int c = 0; c < 10; c++) {
+            neuralNetwork.learn(testValues, testValues);
+            neuralNetwork.learn(testValues2, testValues2);
+            neuralNetwork.learn(testValues3, testValues3);
+            neuralNetwork.learn(testValues4, testValues4);
+        }
+
+        System.out.println("Outputs:");
+        System.out.println(Arrays.toString(neuralNetwork.getOutput(testValues)));
+        System.out.println(Arrays.toString(neuralNetwork.getOutput(testValues2)));
+        System.out.println("Classifications: ");
+        System.out.println(neuralNetwork.getClass(testValues));
+        System.out.println(neuralNetwork.getClass(testValues2));
     }
 }
